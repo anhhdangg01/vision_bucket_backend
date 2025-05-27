0bb717803b991fd6fdc7ca03a87a892359d824bc
Thomas Tran <thomaht3@uci.edu>
2025-05-26 21:11:08 -0700

    Make the server start :DDD

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Hello, World!"
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});

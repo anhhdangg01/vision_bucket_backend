ba781ccb6bb3e7d8dca9efc8e4c4b1f4c31ae1a4
anhhdangg01 <dangaq1@uci.edu>
2025-06-07 00:06:12 -0700

    Implemented incomplete update operation for profile (w/ testing)

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { db, collection, getDocs, doc, getDoc, addDoc, updateDoc, arrayUnion } = require('../firebase'); // Use db from firebase.js
 
+// CREATE http://localhost:5000/profile/data (creates a new user profile data)
+
 // GET http://localhost:5000/profile/data (fetches all user profile data)
 router.get('/data', async (req, res) => {
     try {
@@ -23,5 +25,32 @@ router.get('/data', async (req, res) => {
         res.status(500).json({error: "Failed to fetch user data"});
     }
 });
+
+// // UPDATE http://localhost:5000/profile/data:id (updates an existing user profile data)
+// router.patch('/data/:id', async (req, res) => {
+//     if (req.body.Username != null) {
+//         res.data.Username = req.body.Username
+//     }
+//     if (req.body.Last_online != null) {
+//         res.data.Last_online = req.body.Last_online
+//     }
+//     try {
+//         const querySnapshot = await getDocs(collection(db, "Users"));
+//         const profile = querySnapshot.docs.
+//         const updatedUserData = await res.user_data.save()
+//         res.json(updatedUserData)
+//     } catch (error) {
+//         res.status(400).json({ message: error.message})
+//     }
+// })
+
+// DELETE http://localhost:5000/profile/data/:id (deletes the selected user data based on ID)
+// router.delete('/data/id', async (req, res) => {
+//     try {
+//         profile = await 
+//     } catch {
+
+//     }
+// })
 
 module.exports = router;
